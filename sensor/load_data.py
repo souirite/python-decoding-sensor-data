4fde248506655670ff49f9e06cc3bff938640836
Adil <adil.souirite@gmail.com>
2023-05-03 09:42:17 +0100

    delimiter Module 1

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -6,7 +6,7 @@ def load_sensor_data():
 
     for sensor_file in sensor_files:
         with open(sensor_file,'r') as data_file:
-            data_reader  = csv.DictReader(data_file)
+            data_reader  = csv.DictReader(data_file,delimiter=',')
             for row in data_reader:
                 sensor_data.append(row)
     return sensor_data
