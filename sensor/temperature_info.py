1e9770f699024882ea63d4f92f062df7de3d805e
Adil <adil.souirite@gmail.com>
2023-05-04 12:04:47 +0100

    m3

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -8,10 +8,10 @@ class TemperatureData (HouseInfo):
             recs.append(int(rec, base=10))
         return recs
     
-    def get_data_by_area(self, field, rec_area = 0):
+    def get_data_by_area(self, rec_area = 0):
         recs =super(TemperatureData, self).get_data_by_area("temperature",rec_area )
         return self._convert_data(recs)
     
-    def get_data_by_date(self,field,rec_date= date.today()):
+    def get_data_by_date(self,rec_date= date.today()):
         recs =super(TemperatureData, self).get_data_by_date("temperature",rec_date )
         return self._convert_data(recs)
